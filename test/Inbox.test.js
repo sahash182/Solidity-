675fdfe99be71b27a7a1297f9188633dedd0e78c
Sahash Sainju <sahash182@gmail.com>
2022-01-02 20:00:43 -0800

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,53 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
+
+
+
+
+//MOCHA Example
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+// beforeEach(() => {
+//      car = new Car();
+// });
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
